a5cc2ccbe0ff37621afcfc44e1cef57391679e1f
Steven Truong <svtruong@ualberta.ca>
2022-09-13 14:24:57 -0600

    Readded prints for step 8 in virtualenv and step 5 in curl

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 import requests
 
+print(requests.__version__)
+
+print(requests.get("http://www.google.com/"))
+
 r = (requests.get("https://raw.githubusercontent.com/TheSTruong/cmput404/main/script.py"))
 
 content = r.text
