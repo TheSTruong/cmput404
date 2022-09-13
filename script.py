20790579eb7ae7bf45b8e1c9e6369d81a794fdd5
Steven Truong <svtruong@ualberta.ca>
2022-09-13 14:15:56 -0600

    Script downloads itself and prints source code

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
 import requests
 
-print(requests.get("http://www.google.com/"))
+r = (requests.get("https://raw.githubusercontent.com/TheSTruong/cmput404/main/script.py"))
+
+content = r.text
+
+with open("script.py", "w") as file:
+	file.write(content)
+
+print(content)
